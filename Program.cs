8f2ba3c50887432e5a90f3fccac8bf5e44a4b6d3
tgondorf <tgondorf@classfactory.net>
2020-09-07 17:37:30 +0200

    first updates

--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.EventArgs;
 
 namespace UnicornMissionControlBot
 {
     class Program
     {
-        static void Main(string[] args)
+        //static DiscordClient discord;
+
+        private static string ClientID = "751432550886670467";
+        private static string ClientSecret = "AzmL-A6bJP_nVKrqbBVYUm1b5qmqjQSz";
+
+        private static string Token = "NzUxNDMyNTUwODg2NjcwNDY3.X1JAGQ.hsgky5PWSlSFdfYu9DggF2h48Ks";
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = Program.Token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += OnMessageCreated;
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task OnMessageCreated(MessageCreateEventArgs e)
+        {
+            if (string.Equals(e.Message.Content, "!mc", StringComparison.OrdinalIgnoreCase))
+            {
+                var status =
+                "STATUS (please copy and paste complete message and edit appropriate):";
+
+                var username = e.Message.Author.Username;
+                await e.Message.RespondAsync(status);
+               
+            }
         }
     }
 }
